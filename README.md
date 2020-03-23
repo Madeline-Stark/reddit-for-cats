14cba5c987c20bfe774ce9c46d44acca083e2dfa
github username <github email address>
2020-03-23 15:56:01 -0700

    Build new, edit, destroy. Add Validations. Add partial

---
+++ README.md
@@ -0,0 +1,34 @@
+User
+username
+
+Post
+title 
+content
+user_id
+
+
+User
+has_many :posts
+
+
+Post
+belongs_to :user
+
+
+C
+new get '/posts/new'
+create post '/posts'
+
+R
+index get '/posts'
+show get '/posts/:id'
+
+U
+edit get '/posts/:id/edit'
+update patch '/posts/:id'
+
+D
+delete delete '/posts/:id'
+
+To dos
+- make nested form
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create 
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else 
+      render :edit 
+    end
+  end 
+
+  def destroy 
+    @post.destroy
+    redirect_to posts_path
+  end 
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def current_user
+    User.first
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+
+end
